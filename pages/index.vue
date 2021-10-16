cf660b4e484f9499dd0daf86892b85b48566c139
Kanadech J <kanadech.king@gmail.com>
2021-10-16 17:36:34 +0700

    homepage

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID">
+      <home-card :home="home" />
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from "~/data/homes.json";
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
